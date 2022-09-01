fa3290c9bfd1e82f571c161c02ff37eee90bd513
al-ayubi2020 <al.ayubi2020@gmail.com>
2022-09-01 14:03:55 +0700

    change settings.py

--- settings.py
+++ settings.py
@@ -2,9 +2,7 @@ import os
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-```
 ALLOWED_HOSTS = ["*"]
-```
 
 MIDDLEWARE = [
     ...,
